683e795ed56ed2afe50c71376763e2dc966844a9
ilhamvorta <ilham@vorta.live>
2024-06-08 15:23:11 +0700

    update

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -18,7 +18,7 @@
           <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
             <ul class="navbar-nav">
               <li class="nav-item">
-                <a class="nav-link" href="{{ route('condition.index') }}">Atribut</a>
+                <a class="nav-link" href="{{ route('condition.index') }}">Data Atribut</a>
               </li>
               <li class="nav-item">
                 <a class="nav-link" href="{{ route('condition-data.index') }}">Data</a>
